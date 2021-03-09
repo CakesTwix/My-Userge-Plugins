fc69dd14a83e2b9e75ae27c33bb37f5daef5bc04
CakesTwix <oleg.kiryazov@gmail.com>
2021-03-09 21:37:46 +0200

    plugins:twrp: Add more version

--- plugins/cusrom/twrp.py
+++ plugins/cusrom/twrp.py
@@ -20,16 +20,15 @@ async def device_recovery(message: Message):
     url = get(f"https://dl.twrp.me/{device}/")
     if url.status_code == 404:
         reply = f"`Couldn't find twrp downloads for {device}!`\n"
-        return await message.edit(reply, del_in=5)
     page = BeautifulSoup(url.content, "lxml")
-    download = page.find("table").find("tr").find("a")
-    dl_link = f"https://dl.twrp.me{download['href']}"
-    dl_file = download.text
-    size = page.find("span", {"class": "filesize"}).text
-    date = page.find("em").text.strip()
-    reply = (
-        f"**Latest TWRP for {device}:**\n"
-        f"[{dl_file}]({dl_link}) - __{size}__\n"
-        f"**Updated:** __{date}__"
-    )
+    download = page.find("table").find_all("tr")
+    reply = f"**Team Win Recovery Project for {device}:**\n"
+    for item in download:
+        dl_link = f"https://dl.twrp.me{item.find('a')['href']}"
+        dl_file = item.text
+        size = item.find("span", {"class": "filesize"}).text
+        date = page.find("em").text.strip()
+        reply += (
+            f"⦁ [{dl_file}]({dl_link}) - __{size}__\n"
+            )
     await message.edit(reply)
