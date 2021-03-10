fb5c5123f67b409bdc051b01abdfe31ba947e346
CakesTwix <oleg.kiryazov@gmail.com>
2021-03-10 17:27:30 +0200

    plugins:booru: Add danbooru

--- plugins/anime/booru.py
+++ plugins/anime/booru.py
@@ -23,3 +23,23 @@ async def loli_(message: Message):
                                     photo=answer[0]["sample_url"].replace (" ","%20"),
                                     caption=tag,
                                     disable_notification=True)
+
+
+@userge.on_cmd("danbooru", about={
+    'header': "Anime Arts! From danbooru.donmai.us",
+    'description': 'Sending art from danbooru',
+    'usage': "{tr}danbooru"})
+async def loli_(message: Message):
+    """ Anime Arts """
+    await message.delete()
+    async with aiohttp.ClientSession() as session:
+            async with session.get("https://danbooru.donmai.us/posts.json?limit=1&tags=order:random rating:s") as get:
+                answer = await get.json()
+                await session.close()
+    tag = ''
+    for tags in answer[0]["tag_string"].split():
+                tag += '#' + tags + ' '
+    await message.client.send_photo(chat_id=message.chat.id,
+                                    photo=answer[0]["large_file_url"].replace (" ","%20"),
+                                    caption=tag,
+                                    disable_notification=True)
